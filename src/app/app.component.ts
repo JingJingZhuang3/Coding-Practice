5cb86c7ef201bb88e5ad6b9529f1b412df3e36fd
JingJing Zhuang <jzhuang3219@gmail.com>
2022-08-08 16:30:41 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AlertController, IonInfiniteScroll, IonSlide, IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+    ngOnInit(): void {}
+    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { RouteReuseStrategy, RouterModule } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
+import { IndexComponent } from './index/index.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { MainComponent } from './main/main.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    CartComponent,
+    ProductListComponent,
+    UserLoginComponent,
+    MainComponent,
+    ProductDetailComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,  //Contain directives: ngFor, ngIf...
+    IonicModule.forRoot(),  // Contains <ion-...> tags
+    //AppRoutingModule
+    RouterModule.forRoot([
+      {path: '', redirectTo: 'index', pathMatch:'full'},
+      {path: 'index', component: IndexComponent},
+      {path: 'product-list', component: ProductListComponent},
+      {path: 'cart', component: CartComponent},
+      {path: 'user-login', component: UserLoginComponent},
+      {path: 'product-detail/:pid', component: ProductDetailComponent},
+      {path: '**', component: NotFoundComponent}
+    ]),  //use my own route, not the default one
+    FormsModule,  //ngModel
+    HttpClientModule //contain httpclient
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
